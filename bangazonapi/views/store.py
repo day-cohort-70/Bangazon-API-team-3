e4b20fd40bafcab50ea495b2c1c5395b1edd0ebf
matt novotny <mattrnovotny@gmail.com>
2024-06-26 15:31:56 -0500

    fixed store model reffering to user instead of customer. refactored store view.

--- bangazonapi/views/store.py
+++ bangazonapi/views/store.py
@@ -1,37 +1,36 @@
 from rest_framework import serializers
+from django.db.models import Count
 from django.contrib.auth.models import User
+from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 from bangazonapi.models import Store, Product
-from rest_framework.response import Response
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
-            "id",  # Add this field
+            "id",
             "username",
             "email",
             "first_name",
             "last_name",
-        ]  # Add other fields as needed
+        ]
 
 
 class StoreSerializer(serializers.ModelSerializer):
-    seller = UserSerializer(read_only=True)
-    product_count = serializers.SerializerMethodField()
+    seller = UserSerializer(source="seller.user", read_only=True)
+    product_count = serializers.IntegerField()
 
     class Meta:
         model = Store
         fields = ["id", "name", "description", "seller", "product_count"]
 
-    def get_product_count(self, obj):
-        return Product.objects.filter(store=obj).count()
-
 
 class Stores(ViewSet):
-
     def list(self, request):
-        stores = Store.objects.all()
+        stores = Store.objects.select_related("seller__user").annotate(
+            product_count=Count("products")
+        )
         serializer = StoreSerializer(stores, many=True, context={"request": request})
         return Response(serializer.data)
