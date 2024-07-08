2faddfa255fde1958c661218063b89cbb18d58b7
Carson Ruscin <carson.xlive@gmail.com>
2024-07-08 15:13:43 -0500

    changed message for if order product does not exist

--- bangazonapi/views/lineitem.py
+++ bangazonapi/views/lineitem.py
@@ -99,7 +99,7 @@ class LineItems(ViewSet):
             return Response({}, status=status.HTTP_204_NO_CONTENT)
 
         except OrderProduct.DoesNotExist as ex:
-            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'message': 'Order product not found'}, status=status.HTTP_404_NOT_FOUND)
 
         except Exception as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
