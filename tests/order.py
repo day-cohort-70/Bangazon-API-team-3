95c06793e1089497a9b7092f55ce9314d46dd5c3
Oliver Kennelly <victoriakennelly@gmail.com>
2024-07-12 14:24:32 -0500

    set up now works

--- tests/order.py
+++ tests/order.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 from rest_framework import status
 from rest_framework.test import APITestCase
 
@@ -15,19 +16,33 @@ class OrderTests(APITestCase):
         json_response = json.loads(response.content)
         self.token = json_response["token"]
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.user_id = json_response['id']
+
+        # Create a store
+        url = "/stores"
+        store_data = {"name": "Big Bucks", "description": "a store", "seller": self.user_id, "created_date": datetime.date.today().isoformat()}
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        store_response = self.client.post(url, store_data, format='json')
+        self.assertEqual(store_response.status_code, status.HTTP_201_CREATED)
+        store_json = json.loads(store_response.content)
+        self.store_id = store_json['id']
+
 
         # Create a product category
         url = "/productcategories"
-        data = {"name": "Sporting Goods"}
+        category_data = {"name": "Sporting Goods"}
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
-        response = self.client.post(url, data, format='json')
+        category_response = self.client.post(url, category_data, format='json')
+        self.assertEqual(category_response.status_code, status.HTTP_201_CREATED)
+        category_json = json.loads(category_response.content)
+        self.category_id = category_json['id']
 
         # Create a product
         url = "/products"
-        data = { "name": "Kite", "price": 14.99, "quantity": 60, "description": "It flies high", "category_id": 1, "location": "Pittsburgh" }
+        product_data = { "name": "Kite", "price": 14.99, "quantity": 60, "description": "It flies high", "category_id": self.category_id, "location": "Pittsburgh", "store_id": self.store_id }
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        product_response = self.client.post(url, product_data, format='json')
+        self.assertEqual(product_response.status_code, status.HTTP_201_CREATED)
 
 
     def test_add_product_to_order(self):
@@ -84,13 +99,26 @@ class OrderTests(APITestCase):
         Ensure that an order can be completed with a PUT request containing the payment ID
         """
         #add item to cart
-        self.test_add_product_to_order
+        self.test_add_product_to_order()
+
+        #create a payment type
+        url = "/paymenttypes"
+        data = {
+        "merchant_name": "American Express",
+        "account_number": "111-1111-1111",
+        "expiration_date": "2024-12-31",
+        "create_date": datetime.date.today().isoformat()
+    }
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        payment_id = response.json()['id']
 
         #add payment
         url = "/orders/1"
-        data = { "payment_type": 1 }
+        data = { "payment_type": payment_id }
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
-        response = self.client.post(url, data, format='json')
+        response = self.client.put(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
         #fetch the order again to verify changes were made
@@ -99,5 +127,5 @@ class OrderTests(APITestCase):
 
         #assert that the properties are correct
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(json_response["payment_type"], 1)
+        self.assertEqual(json_response["payment_type"], payment_id)
     # TODO: New line item is not added to closed order
